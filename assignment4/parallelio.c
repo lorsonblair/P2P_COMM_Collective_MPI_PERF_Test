c19129810b71e55dce8bbfe58f862184625b0c47
lorsonblair <56801497+lorsonblair@users.noreply.github.com>
2020-04-30 20:15:37 -0400

    working code. malloc.

--- assignment4/parallelio.c
+++ assignment4/parallelio.c
@@ -1,17 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "mpi.h"
+#include<unistd.h>
 
-#define K 1000
-#define M 1000000
 #define NUM_BLOCKS 64
+#define FREQUENCY 512000000
 
-#define BLOCK_SIZE 128 * K
+/****************************************************************/
+// FOR POWER9 SYSTEMS ONLY - x86 SYSTEMS HAVE A DIFFERENT CODE  //
+/****************************************************************/
+
+typedef unsigned long long ticks;
+
+static __inline__ ticks getticks(void)
+{
+  unsigned int tbl, tbu0, tbu1;
+
+  do {
+    __asm__ __volatile__ ("mftbu %0" : "=r"(tbu0));
+    __asm__ __volatile__ ("mftb %0" : "=r"(tbl));
+    __asm__ __volatile__ ("mftbu %0" : "=r"(tbu1));
+  } while (tbu0 != tbu1);
+
+  return (((unsigned long long)tbu0) << 32) | tbl;
+}
 
 int main(int argc, char **argv)
 {
     int myrank, nprocs, block_size;
-    char *buffer = malloc(BLOCK_SIZE);
+    
+    char file_name[64];
+
+    unsigned long long start = 0;
+    unsigned long long finish = 0;
+    unsigned long long result = 0;
+    float time = 0.0;
+    
+    if (argc != 2)
+    {
+      printf("Block size required\n");
+      return (EXIT_FAILURE);
+    }
+
+    block_size = atoi(argv[1]);
+    char *buffer = malloc(block_size);
 
     MPI_File file;
     MPI_Offset offset;
@@ -22,31 +54,75 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);     // my rank. current rank
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);     // number of ranks (processes)
     
-    //offset = myrank * BLOCK_SIZE;               // start of the view for each processor 
-    //count = BLOCK_SIZE;             // count is basically the block size
-    
     // fill the buffer with all 1s
-    for (int i = 0; i < BLOCK_SIZE; i++) buffer[i] = '1';
+    for (int i = 0; i < block_size; i++) buffer[i] = '1';
+
+    sprintf(file_name, "%d_ranks_%d_blksz", nprocs, block_size);
     
+    if (myrank == 0) 
+    {
+        printf("***********************************\n");
+        printf("Parallel IO Tests for %s\n", file_name);
+	printf("Write Test\n");
+        start = getticks();
+    }
+    
+    MPI_File_open(MPI_COMM_WORLD, file_name, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &file);
     // perform NUM_BLOCKS writes 
     for (int i = 0; i < NUM_BLOCKS; i++)
     {
-        offset = i * BLOCK_SIZE;
-        MPI_File_open(MPI_COMM_WORLD, "datafile.out", MPI_MODE_WRONLY, MPI_INFO_NULL, &file);
-        MPI_File_write_at(file, offset, buffer, BLOCK_SIZE, MPI_CHAR, &status);
-        MPI_File_close(&file);
+        offset = (myrank * block_size) + (i * nprocs * block_size);
+        MPI_File_write_at(file, offset, buffer, block_size, MPI_CHAR, &status);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_File_close(&file);
+    
+    // stop timer and print result
+    if (myrank == 0)
+    {
+        finish = getticks();
+        result = finish - start;
+	time = (float)result / FREQUENCY;
+	printf("Start Ticks: %llu\n", start);
+	printf("Finish Ticks: %llu\n", finish); 
+        printf("Result: %llu\n", result);
+	  
+        printf("Write Time (s): %.3f\n", time);
+    } 
+
+    // start timer for reads
+    if (myrank == 0) 
+    {
+        printf("\nRead Test\n");
+        start = getticks();
     }
 
     // perform NUM_BLOCKS reads
+    MPI_File_open(MPI_COMM_WORLD, file_name, MPI_MODE_RDONLY, MPI_INFO_NULL, &file);
     for (int i = 0; i < NUM_BLOCKS; i++)
     {
-        MPI_File_open(MPI_COMM_WORLD, "datafile.out", MPI_MODE_RDONLY, MPI_INFO_NULL, &file);
-        MPI_File_read_at(file, offset, buffer, BLOCK_SIZE, MPI_CHAR, &status);    
-        MPI_File_close(&file);
+        offset = (myrank * block_size) + (i * nprocs * block_size);
+        MPI_File_read_at(file, offset, buffer, block_size, MPI_CHAR, &status);    
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_File_close(&file);
+
+    // stop timer and print result
+    if (myrank == 0)
+    {
+        finish = getticks();
+        result = finish - start;
+	time = (float)result / FREQUENCY;
+	printf("Start Ticks: %llu\n", start);
+	printf("Finish Ticks: %llu\n", finish); 
+        printf("Result: %llu\n", result);
+	printf("Read Time (s): %.3f\n", time);
+
+        printf("***********************************\n");
     }
     
+    // clean up
     MPI_Finalize();
     free(buffer);
     return 0;
-    
 }
