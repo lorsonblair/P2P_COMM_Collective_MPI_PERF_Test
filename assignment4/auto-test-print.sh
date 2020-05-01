1ccf692ad5b359da8007fc59c26de0bd2c738240
ItoSchum <itoschum@gmail.com>
2020-05-01 03:38:59 -0700

    Second round test for non-CUDA runtime test individually, treating 1M as 1,000,000

--- assignment4/auto-test-print.sh
+++ assignment4/auto-test-print.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 rm ./slurm-* ./*_blksz; sbatch -N 1 --ntasks-per-node=2 --gres=gpu:1 -t 30 ./slurmSpectrum.sh
-rm ./slurm-* ./*_blksz; sbatch -N 1 --ntasks-per-node=4 --gres=gpu:1 -t 30 ./slurmSpectrum.sh
+sleep 3s
+
+rm ./*_blksz; sbatch -N 1 --ntasks-per-node=4 --gres=gpu:1 -t 30 ./slurmSpectrum.sh
+sleep 3s
+
 rm ./*_blksz; sbatch -N 1 --ntasks-per-node=8 --gres=gpu:1 -t 30 ./slurmSpectrum.sh
+sleep 6s
+
 rm ./*_blksz; sbatch -N 1 --ntasks-per-node=16 --gres=gpu:1 -t 30 ./slurmSpectrum.sh
+sleep 30s
+
 rm ./*_blksz; sbatch -N 1 --ntasks-per-node=32 --gres=gpu:1 -t 30 ./slurmSpectrum.sh
+sleep 30s
+
 rm ./*_blksz; sbatch -N 2 --ntasks-per-node=32 --gres=gpu:1 -t 30 ./slurmSpectrum.sh
+sleep 30s
+rm ./*_blksz
+
+bash ./auto-print.sh
--- assignment4/auto-test.sh
+++ assignment4/auto-test.sh
@@ -5,6 +5,10 @@ rm ./slurm-* ./*_blksz
 for i in 2 4 8 16 32
 do
     sbatch -N 1 --ntasks-per-node="$i" --gres=gpu:1 -t 30 ./slurmSpectrum.sh
+    sleep 30s
+    rm ./*_blksz
 done
 
 sbatch -N 2 --ntasks-per-node=32 --gres=gpu:1 -t 30 ./slurmSpectrum.sh
+sleep 30s
+rm ./*_blksz
